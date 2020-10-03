7e6856e50f0f45bba5e132a3b73783b8dde73491
azer <azerilgar@gmail.com>
2020-10-03 20:18:10 +0300

    make it better

--- server.cjs
+++ server.cjs
@@ -11,7 +11,7 @@
 
   var yargs = require("yargs").options({
     port: {
-      default: 8080,
+      default: process.env.PORT || 8080,
       description: "Port to listen on.",
     },
     public: {
@@ -188,7 +188,7 @@
 
   var server = app.listen(
     argv.port,
-    argv.public ? undefined : "0.0.0.0",
+    argv.public ? undefined : "localhost",
     function () {
       if (argv.public) {
         console.log(
